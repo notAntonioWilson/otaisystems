6c2f1bc7bc8243f530b5cc1b5c7b9e8834df3235
theprofessional <210361783+notAntonioWilson@users.noreply.github.com>
2026-02-22 20:57:52 -0500

    Updated tailwind.config.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,400&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <CursorTrail />
